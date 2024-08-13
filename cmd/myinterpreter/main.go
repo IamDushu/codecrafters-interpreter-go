5c7d5c4719f210e1cbb2ade5a408188f5fe1d613
Dushyanth <dushu@Dushyanths-MacBook-Air.local>
2024-08-13 22:18:40 +0530

    Scanning braces

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -9,6 +9,8 @@ func main() {
 	lexemeMap := map[string]rune{
 		"LEFT_PAREN":  '(',
 		"RIGHT_PAREN": ')',
+		"LEFT_BRACE":  '{',
+		"RIGHT_BRACE": '}',
 	}
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
@@ -42,8 +44,11 @@ func main() {
 			fmt.Printf("LEFT_PAREN %v null\n", string(lexemeMap["LEFT_PAREN"]))
 		case lexemeMap["RIGHT_PAREN"]:
 			fmt.Printf("RIGHT_PAREN %v null\n", string(lexemeMap["RIGHT_PAREN"]))
+		case lexemeMap["LEFT_BRACE"]:
+			fmt.Printf("LEFT_BRACE %v null\n", string(lexemeMap["LEFT_BRACE"]))
+		case lexemeMap["RIGHT_BRACE"]:
+			fmt.Printf("RIGHT_BRACE %v null\n", string(lexemeMap["RIGHT_BRACE"]))
 		}
 	}
 	fmt.Println("EOF  null")
-
 }
